7bbe72fece87c6e70f7b8cc48cbe037b72d24259
Tautminas Cibulskis <tautminas@yahoo.com>
2023-10-01 23:18:06 +0300

    Add template

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "<h1 style='text-align:center;'>Hello, let's track some goals!</h1>"
+    return render_template("index.html")
 
 
 @app.route("/<goal>/<int:days>")
-def greet(goal, days):
+def goals(goal, days):
     return f"<h1 style='text-align:center;'>Your goal: {goal} <br /> Days to reach it: {days}</h1>"
 
 
