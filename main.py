83a61f65518167d3603a7625281604a211f228a3
Tautminas Cibulskis <tautminas@yahoo.com>
2023-10-02 22:53:57 +0300

    Create registration form

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import inspect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Email
 import os
 
 app = Flask(__name__)
@@ -23,8 +26,42 @@ with app.app_context():
         db.create_all()
 
 
+class RegistrationForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+
+# User registration route
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        surname = form.surname.data
+        email = form.email.data
+        password = form.password.data
+
+        # Check if the email already exists
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            flash('This email address is already registered. Please use a different email address.', 'danger')
+            return redirect(url_for('register'))
+
+        # Create a new user and add them to the database
+        new_user = User(name=name, surname=surname, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Registration successful! You can now log in.', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+
 @app.route("/")
-def hello_world():
+def home():
     return render_template("index.html")
 
 
