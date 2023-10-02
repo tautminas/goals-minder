9bd5f46d65e7c12076a3b5faa40126bcc9c5c45d
Tautminas Cibulskis <tautminas@yahoo.com>
2023-10-02 22:06:51 +0300

    Create users database

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import inspect
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    surname = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
+
+with app.app_context():
+    inspector = inspect(db.engine)
+    if not inspector.has_table('user'):
+        db.create_all()
 
 
 @app.route("/")
